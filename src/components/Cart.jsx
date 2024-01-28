bbb5403d6aedccf61bf85b58e81b02e424362f27
delia-pixel <tpatriciareinedia@gmail.com>
2024-01-28 16:54:19 +0100

    rewriting with context Api

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,9 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import { CartContext } from "../store/shopping-cart-context";
+import { useContext } from "react";
+
+export default function Cart() {
+  const { items, updateCartItem } = useContext(CartContext);
+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
@@ -20,13 +25,9 @@ export default function Cart({ items, onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
-                    -
-                  </button>
+                  <button onClick={() => updateCartItem(item.id, -1)}>-</button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
-                    +
-                  </button>
+                  <button onClick={() => updateCartItem(item.id, 1)}>+</button>
                 </div>
               </li>
             );
